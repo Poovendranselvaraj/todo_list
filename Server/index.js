626d26f640e21b521445eb2f6023dd25ca8d0b99
poove <poovendranselavaraj333@gmail.com>
2024-08-09 12:31:50 +0530

    error fixed

--- Server/index.js
+++ Server/index.js
@@ -8,14 +8,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb+srv://poovendranselvaraj333:poove@cluster0.waobb.mongodb.net/')
 
 app.post('/add', (req, res) => {
-    const {task} = req.body.task;
+    const task = req.body.task;
     TodoModel.create({
         task: task
-    }).then(result => res)
-     
+    }).then(result => res.json(result))
+    .catch(err=> console.log(err))   
 })
 
 app.listen(3001, () => {
